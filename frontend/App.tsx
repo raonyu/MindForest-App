369dd196b42df1d004ff9809112eeb755cb927fa
HWANCHUL YU <53122601+Cfghle@users.noreply.github.com>
2026-03-09 18:46:38 +0900

    프론트엔드 폴더 추가함
    frontend 폴더 추가

---
+++ frontend/App.tsx
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import React from 'react';
+import { StatusBar, StyleSheet, useColorScheme, View,Text, Pressable, TouchableOpacity } from 'react-native';
+
+//버튼 선언
+const CustomButton = () =>(
+  <TouchableOpacity style = {styles.baseButton}><Text>버튼임</Text></TouchableOpacity>
+
+
+);
+
+function App(){
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View>
+      <Text style={styles.bigfont}>마음의 숲 프로젝트</Text>
+      <CustomButton></CustomButton>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bigfont: {
+    fontSize: 32,
+    fontWeight: 'bold'
+  },
+  baseButton: {
+    backgroundColor: "gray",
+  }
+});
+
+
+
+
+export default App;
