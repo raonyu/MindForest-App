65f1a214b9ed296ee327230e45605b1ab84ebe6d
HWANCHUL YU <53122601+Cfghle@users.noreply.github.com>
2026-03-10 20:26:28 +0900

    네비게이션 UI 구현
    react-navigation 라이브러리를 통해 구현함, 감정일기 <-> 메인 <-> 채팅 구조로 되어 있음

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -6,7 +6,21 @@
  */
 import React from 'react';
 import { StatusBar, StyleSheet, useColorScheme, View,Text, Pressable, TouchableOpacity } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';//네비게이션 라이브러리 불러오기
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 
+//각 화면 컴포넌트들(달력, 매인, 채팅) 
+const ChatScreen = () => (
+  <View><Text>채팅 화면이 뜰겁니다</Text></View>
+);
+const MainScreen = () => (
+  <View><Text>여기는 메인화면입니다</Text></View>
+);
+const DiaryScreen = () => (
+  <View><Text>감정일기 달력 화면</Text></View>
+);
+//텝 컴포넌트 생성
+const Tab = createMaterialTopTabNavigator();
 //버튼 선언
 const CustomButton = () =>(
   <TouchableOpacity style = {styles.baseButton}><Text>버튼임</Text></TouchableOpacity>
@@ -18,11 +32,13 @@ function App(){
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <View>
-      <Text style={styles.bigfont}>마음의 숲 프로젝트</Text>
-      <CustomButton></CustomButton>
-      <CustomButton></CustomButton>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName = "MainScreen">
+        <Tab.Screen name="감정일기" component={DiaryScreen} />
+        <Tab.Screen name="마음의 숲" component={MainScreen} />
+        <Tab.Screen name="채팅" component={ChatScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
