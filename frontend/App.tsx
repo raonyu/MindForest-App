bd88caec9a10693cda73ed7690dd4c4ade680b42
HWANCHUL YU <53122601+Cfghle@users.noreply.github.com>
2026-03-11 23:10:45 +0900

    채팅방 뼈대 구현
    기본적인 메세지 구조
    - `id`: 메세지 식별자
    - `text`: 메세지의 내용이 들어간다
    - `sender`:누가 메세지를 보냈는지
    	- `ai`: ai의 채팅
    	- `user`: 사용자의 채팅
    - `time`: 메세지를 보낸 시각

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -5,20 +5,48 @@
  * @format
  */
 import React from 'react';
-import { StatusBar, StyleSheet, useColorScheme, View,Text, Pressable, TouchableOpacity } from 'react-native';
+import {useState} from 'react';
+import { StatusBar, StyleSheet, useColorScheme, View, Text, Pressable, TouchableOpacity, FlatList, ListRenderItem} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';//네비게이션 라이브러리 불러오기
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 
+//메세지 구조 잡기
+interface Message {
+  id: string;
+  text: string;
+  sender: 'user' | 'ai';
+  time: string;
+}
+
+const MOCK_MESSAGES: Message[] = [
+	{id: '1', text: "상대방 매세지", sender: 'ai', time: '오전 10:00'},
+	{id: '2', text: "나의 메세지", sender: 'user', time: '오전 11:00'}
+];
+
+
 //각 화면 컴포넌트들(달력, 매인, 채팅) 
-const ChatScreen = () => (
+const ChatScreen = () => {
+  const [messages, setMessages] = useState<Message[]>(MOCK_MESSAGES);
+  const [inputText, setInputText] = useState<string>('');
+
+  //메세지 렌더링 컴포넌트
+  const renderMessages: ListRenderItem<Message> = ({item}: {item: Message}) => (
+    <View>
+      <Text>{item.text}</Text>
+      <Text>{item.time}</Text>
+    </View>
+  );
+  return (
   <View>
     <Text>채팅 화면이 뜰겁니다</Text>
-  
-  
-  
-  
+    <FlatList<Message>
+      data={messages}
+      renderItem= {renderMessages}
+      keyExtractor={item => item.id}
+    />  
   </View>
-);
+  );
+};
 const MainScreen = () => (
   <View><Text>여기는 메인화면입니다</Text></View>
 );
@@ -63,7 +91,7 @@ const styles = StyleSheet.create({
   baseButton: {
     backgroundColor: "gray",
   },
-tabBar: {
+  tabBar: {
     backgroundColor: 'green',
   }   
 });
